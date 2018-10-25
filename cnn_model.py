12dbef64170e5fb1ba36f0007f9e7c3e3517451e
Remo Storni <remo.storni@gmail.com>
2018-10-25 10:00:15 -0700

    Second update. Improved api and added documentation

--- cnn_model.py
+++ cnn_model.py
@@ -7,68 +7,91 @@ import torch.optim as optim
 class Model(nn.Module):
     def __init__(self,params):
         super(Model, self).__init__()
-        
+        # how often (in the number of batches) to print loss and save the model during training
+        self.log_interval = 1000
         # here we assume that input media can only be square
         self.input_size = 28
-        
+        default_params ={
+            'max_pool_kernel_size':2,
+            'max_pool_stride':2,
+            'max_pool_padding':0,
+            
+            'conv1_kernel_size':3,
+            'conv1_stride':1,
+            'conv1_padding':1,
+            'conv1_in_channels':1,
+            'conv1_out_channels':32,
+
+            'conv2_kernel_size':3,
+            'conv2_stride':1,
+            'conv2_padding':1,
+            'conv2_in_channels':32,
+            'conv2_out_channels':64,
+
+            'conv3_kernel_size':3,
+            'conv3_stride':1,
+            'conv3_padding':1,
+            'conv3_in_channels':64,
+            'conv3_out_channels': 256,
+    
+            'linear1_output':1000,
+            'linear2_output':500,
+            
+            'learning_rate':0.005,
+            'momentum':0.5
+        }
+        default_params.update(params)
+
         max_pool_param = {
-            'kernel_size':params['max_pool_kernel_size'],
-            'stride':params['max_pool_stride'],
-            'padding':params['max_pool_padding']
+            'kernel_size':default_params['max_pool_kernel_size'],
+            'stride':default_params['max_pool_stride'],
+            'padding':default_params['max_pool_padding']
             }
         self.pooling = nn.MaxPool2d(**max_pool_param)
         
         self.conv1 = {
-            'kernel_size':params['conv1_kernel_size'],
-            'padding':params['conv1_padding'],
-            'stride':params['conv1_stride'],
-            'in_channels':params['conv1_in_channels'],
-            'out_channels':params['conv1_out_channels']
+            'kernel_size':default_params['conv1_kernel_size'],
+            'padding':default_params['conv1_padding'],
+            'stride':default_params['conv1_stride'],
+            'in_channels':default_params['conv1_in_channels'],
+            'out_channels':default_params['conv1_out_channels']
             }
-
         c1_dim = (self.input_size-self.conv1['kernel_size']+2*self.conv1['padding'])/self.conv1['stride']+1
         c1_pooled_dim = (c1_dim-max_pool_param['kernel_size'])/max_pool_param['stride'] + 1
         self.conv1_layer = nn.Conv2d(**self.conv1)
         
         self.conv2 = {
-            'kernel_size':params['conv2_kernel_size'],
-            'padding':params['conv2_padding'],
-            'stride':params['conv2_stride'],
-            'in_channels':params['conv2_in_channels'],
-            'out_channels':params['conv2_out_channels']
+            'kernel_size':default_params['conv2_kernel_size'],
+            'padding':default_params['conv2_padding'],
+            'stride':default_params['conv2_stride'],
+            'in_channels':default_params['conv2_in_channels'],
+            'out_channels':default_params['conv2_out_channels']
             }
-
         if self.conv2['in_channels'] != self.conv1['out_channels']:
             raise
-
-        
         c2_dim = (c1_pooled_dim-self.conv2['kernel_size']+2*self.conv2['padding'])/self.conv2['stride']+1
         c2_pooled_dim = (c2_dim-max_pool_param['kernel_size'])/max_pool_param['stride'] + 1
         self.conv2_1_layer = nn.Conv2d(**self.conv2)
         self.conv2_2_layer = nn.Conv2d(**self.conv2)
         
-        
         self.conv3 = {
-            'kernel_size':params['conv3_kernel_size'],
-            'padding':params['conv3_padding'],
-            'stride':params['conv3_stride'],
-            'in_channels':params['conv3_in_channels'],
-            'out_channels':params['conv3_out_channels']
+            'kernel_size':default_params['conv3_kernel_size'],
+            'padding':default_params['conv3_padding'],
+            'stride':default_params['conv3_stride'],
+            'in_channels':default_params['conv3_in_channels'],
+            'out_channels':default_params['conv3_out_channels']
             }
-
         if self.conv3['in_channels'] != self.conv2['out_channels']:
             raise
-
         self.c3_dim = int((c2_pooled_dim-self.conv3['kernel_size']+2*self.conv3['padding'])/self.conv3['stride']+1)
         self.conv3_1_layer = nn.Conv2d(**self.conv3)
         self.conv3_2_layer = nn.Conv2d(**self.conv3)
         
-
-        self.linear1 = nn.Linear(int(self.conv3['out_channels']*2*self.c3_dim*self.c3_dim), params['linear1_output'])
+        self.linear1 = nn.Linear(int(self.conv3['out_channels']*2*self.c3_dim*self.c3_dim), default_params['linear1_output'])
         
-
-        self.linear2 = nn.Linear(params['linear1_output'], params['linear2_output'])
-        self.out = nn.Linear(params['linear2_output'],10)
+        self.linear2 = nn.Linear(default_params['linear1_output'], default_params['linear2_output'])
+        
+        self.out = nn.Linear(default_params['linear2_output'],10)
         
 
     def forward(self, x):
@@ -91,81 +114,86 @@ class Model(nn.Module):
         o = self.out(l2)
         return F.log_softmax(o)
 
-def train_model(epoch,model,optimizer,train_loader):
-    log_interval = 1000
-    model.train()
-    max_ct = 3
-    ct = 0
-    for batch_idx, (data, target) in enumerate(train_loader):
-        if ct > max_ct:
-            break
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % log_interval == 0:
-            ct = ct + 1
-            print("Train epoch: {} [{}/{} ({:.0f}%)]\t Loss: {:.6f}".format(
-                    epoch, batch_idx*len(data),len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss.item()))
-            torch.save(model.state_dict(), './results/model.pth')
-            torch.save(optimizer.state_dict(), './results/optimizer.pth')
+    def train_model(self,epoch,optimizer,train_loader):
+        """
+        input: epoch (used during printing)
+        input: optimizer
+        input: train_loader, iterable on tuples (data, label)
+        return: void
+        """
+        self.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            optimizer.zero_grad()
+            output = self(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % self.log_interval == 0:
+                print("Train epoch: {} [{}/{} ({:.0f}%)]\t Loss: {:.6f}".format(
+                        epoch, batch_idx*len(data),len(train_loader.dataset),
+                        100. * batch_idx / len(train_loader), loss.item()))
+                torch.save(self.state_dict(), './results/model.pth')
+                torch.save(optimizer.state_dict(), './results/optimizer.pth')
 
-def test_model(model,test_loader):
+    def eval_model(self,test_loader):
+        """
+        input: test_loader, iterable on tuples (data, label)
+        return: dict with metrics
+        """
+        metrics = {}
+        self.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                output = self(data)
+                test_loss += F.nll_loss(output, target, size_average=False).item()
+                pred = output.data.max(1, keepdim=True)[1]
+                correct += pred.eq(target.data.view_as(pred)).sum()
+            test_loss /= len(test_loader.dataset)
+            print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+                    test_loss, correct, len(test_loader.dataset),
+                    100. * correct / len(test_loader.dataset)))
+            metrics['avg_loss'] = test_loss
+            metrics['accuracy'] = float(correct)/float(len(test_loader.dataset))
+        return metrics
+
+    def default_optimizer(self, learning_rate = 0.005, momentum=0.5):
+        return optim.SGD(self.parameters(), learning_rate,momentum)
+
+    def run_train_and_eval(self, optimizer, n_epochs, test_loader, train_loader):
+        """
+        input: optimizer
+        input: n_epochs, number of passes on training_data
+        input: test_loader, iterable on tuples (data, label)
+        input: train_loader, iterable on tuples (data, label)
+        return: dict with test metrics
+        """
+        self.eval_model(test_loader)
+        test_metrics = None
+        for epoch in range(1, n_epochs + 1):
+            self.train_model(epoch, optimizer, train_loader)
+            test_metrics = self.eval_model(test_loader)
+        return test_metrics
+
+def hyperpameter_tuning(test_loader, train_loader, parameters, metric, maximize = True):
     """
     input: test_loader
-    returns: dict with metrics
-    """
-    metrics = {}
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
-            output = model(data)
-            test_loss += F.nll_loss(output, target, size_average=False).item()
-            pred = output.data.max(1, keepdim=True)[1]
-            correct += pred.eq(target.data.view_as(pred)).sum()
-    test_loss /= len(test_loader.dataset)
-    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    test_loss, correct, len(test_loader.dataset),
-    100. * correct / len(test_loader.dataset)))
-    metrics['avg_loss'] = test_loss
-    metrics['accuracy'] = correct/len(test_loader.dataset)
+    input: train_loader
+    input: parameters, dict containing values, ranges or lists for parameters
+    input: metric, string representing a test metric to optimizer, currently needs to be in [accuracy, avg_loss]
+    input: maximize, whether we want to maximize the metric
 
-
-def default_optimizer(model, learning_rate = 0.005, momentum=0.5):
-    return optim.SGD(model.parameters(), lr=learning_rate,
-                      momentum=momentum)
-
-
-def run_train_and_eval(model, optimizer, n_epochs, test_loader, train_loader):
-    n_epochs = 1
-    test_model(model, test_loader)
-    test_metrics = None
-    for epoch in range(1, n_epochs + 1):
-        train_model(epoch, model, optimizer, train_loader)
-        test_metrics = test_model(model, test_loader)
-    return test_metrics
-
-def hyperpameter_tuning(test_loader, train_loader, metric, parameters):
-    """
-    parameters contains ranges/ lists for parameters to tune and default values for parameters to keep steady
-    metric specifies what metric to optimize on
-
-    returns best parameters set with best metric
-    can print metrics for various parameters
+    return: (best metric found, parameters that generated best metric)
     """
     parameters_list = generate_parameters(parameters)
     best_so_far = None
     best_metric_so_far = None
     for parameters in parameters_list:
-        # model = create model
         nm = Model(parameters)
-        optimizer = default_optimizer(nm, parameters['learning_rate'],parameters['momentum'])
-        metrics = run_train_and_eval(nm, optimizer, 2, test_loader, train_loader)
-        if best_metric_so_far is None or best_metric_so_far < metrics[metric]:
+        optimizer = nm.default_optimizer(parameters['learning_rate'],parameters['momentum'])
+        metrics = nm.run_train_and_eval(optimizer, 2, test_loader, train_loader)
+        if best_metric_so_far is None or (not maximize and best_metric_so_far < metrics[metric]) or (maximize and best_metric_so_far > metrics[metric]):
             best_metric_so_far = metrics[metric]
             best_so_far = parameters
     return (best_metric_so_far, best_so_far)
